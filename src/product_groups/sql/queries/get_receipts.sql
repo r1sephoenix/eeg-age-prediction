719a72000d466a8723fbd87622bf1f3bf2518049
ilyamikheev <r1sephoenix@yandex.ru>
2025-04-19 19:07:11 +0300

    /

--- src/product_groups/sql/queries/get_receipts.sql
+++ src/product_groups/sql/queries/get_receipts.sql
@@ -1,57 +1,56 @@
--- Назначение: Получить чеки за период с фильтрацией по store_id или городу
--- Аргументы: start_date, end_date, store_id (опц.), city (опц.), checkout_items
--- Внимательно!: store_id и city не могут быть заданы одновременно!
+-- Назначение: Получить информацию по чекам за два периода с фильтрацией по товарам и по store_id или городу
+-- Аргументы: start_date_current, end_date_current, start_date_lastyear, end_date_lastyear, store_id (опц.), city (опц.), checkout_items_list
+-- Внимание: store_id и city не могут быть заданы одновременно!
 
 SELECT *
 FROM (
-    SELECT 
-        r.receipt_surrogate_id,  
-        r.line_item_id,
+    SELECT
+        r.receipt_surrogate_id,
         r.client_type,
-        r.client_loyalty_card_number AS client_card,
-        r.opened_date,
-        r.store_id,
+        r.line_item_id,
         i.model_id,
         i.dept,
         i.subdept,
         i.class_id,
         i.subclass,
-        toYYYYMM(r.opened_date) AS month_id,
-        COUNT(DISTINCT line_item_id) OVER (PARTITION BY receipt_surrogate_id) AS count_items,
+        r.store_id,
+        r.opened_date,
+        COUNT(DISTINCT r.line_item_id) OVER (PARTITION BY r.receipt_surrogate_id) AS count_items,
         SUM(r.line_quantity) AS line_quantity
-    FROM assortment_ods.dds_receipt_lines_public AS r 
-    LEFT JOIN assortment_ods.items AS i ON r.line_item_id = i.item 
+    FROM assortment_ods.dds_receipt_lines_public AS r
+    LEFT JOIN assortment_ods.items AS i ON r.line_item_id = i.item
     {% if city %}
     LEFT JOIN assortment_ods.store AS s ON r.store_id = s.store
     {% endif %}
-    WHERE r.opened_date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date
+    WHERE (
+        r.opened_date BETWEEN '{{ start_date_lastyear }}'::date AND '{{ end_date_lastyear }}'::date
+        OR r.opened_date BETWEEN '{{ start_date_current }}'::date AND '{{ end_date_current }}'::date
+    )
     {% if store_id %}
       AND r.store_id {% if store_id is iterable and store_id is not string %}IN ({{ store_id | join(", ") }}){% else %}= {{ store_id }}{% endif %}
     {% endif %}
     {% if city %}
       AND s.district_name = '{{ city }}'
     {% endif %}
-    AND r.line_item_id IN (
-        SELECT item AS line_item_id
-        FROM assortment_ods.items
-        WHERE 1=1
-            AND item_desc NOT LIKE 'CR_%'
-            AND gamma IN ('A', 'Ac', 'A0', 'B')
-            AND line_item_id::int NOT IN ({{ checkout_items | join(", ") }})
-            AND subdept <> 940
-    )
-    GROUP BY 
-        r.receipt_surrogate_id,  
-        r.line_item_id,
+      AND r.line_item_id IN (
+            SELECT item AS line_item_id
+            FROM assortment_ods.items
+            WHERE 1=1
+              AND item_desc NOT LIKE 'CR_%'
+              AND gamma IN ('A', 'Ac', 'A0', 'B')
+              AND line_item_id::int NOT IN ({{ checkout_items_list | join(", ") }})
+              AND subdept <> 940
+       )
+    GROUP BY
+        r.receipt_surrogate_id,
         r.client_type,
-        r.client_loyalty_card_number,
-        r.opened_date,
+        r.line_item_id,
         r.store_id,
+        r.opened_date,
         i.model_id,
         i.dept,
         i.subdept,
         i.class_id,
-        i.subclass,
-        toYYYYMM(r.opened_date)
+        i.subclass
 )
-WHERE count_items >= 2;
+WHERE count_items >= 3;
---
+++ src/product_groups/sql/queries/get_receipts_with_turnover.sql
@@ -0,0 +1,82 @@
+-- Назначение: Выгрузить чеки с деталями по клиентам, товарам, total_turnover и общим оборотом по магазину
+-- Аргументы: start_date, end_date, store_id (опц.), city (опц.), checkout_items
+
+WITH item_turnover AS (
+    SELECT
+        line_item_id,
+        SUM(line_turnover) AS total_turnover
+    FROM assortment_ods.dds_receipt_lines_public
+    WHERE opened_date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date
+      AND line_item_id::int NOT IN ({{ checkout_items | join(", ") }})
+    GROUP BY line_item_id
+),
+store_total_turnover AS (
+    SELECT
+        store_id,
+        SUM(line_turnover) AS store_turnover
+    FROM assortment_ods.dds_receipt_lines_public
+    WHERE opened_date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date
+      AND line_item_id::int NOT IN ({{ checkout_items | join(", ") }})
+    GROUP BY store_id
+)
+
+SELECT *
+FROM (
+    SELECT
+        r.receipt_surrogate_id,
+        r.line_item_id,
+        r.client_type,
+        r.client_loyalty_card_number AS client_card,
+        r.opened_date,
+        r.store_id,
+        i.model_id,
+        i.dept,
+        i.subdept,
+        i.class_id,
+        i.subclass,
+        toYYYYMM(r.opened_date) AS month_id,
+        COUNT(DISTINCT r.line_item_id) OVER (PARTITION BY r.receipt_surrogate_id) AS count_items,
+        SUM(r.line_quantity) AS line_quantity,
+        SUM(r.line_turnover) AS line_turnover,
+        it.total_turnover,
+        st.store_turnover
+    FROM assortment_ods.dds_receipt_lines_public AS r
+    LEFT JOIN assortment_ods.items AS i ON r.line_item_id = i.item
+    LEFT JOIN item_turnover AS it ON r.line_item_id = it.line_item_id
+    LEFT JOIN store_total_turnover AS st ON r.store_id = st.store_id
+    {% if city %}
+    LEFT JOIN assortment_ods.store AS s ON r.store_id = s.store
+    {% endif %}
+    WHERE r.opened_date BETWEEN '{{ start_date }}'::date AND '{{ end_date }}'::date
+    {% if store_id %}
+      AND r.store_id {% if store_id is iterable and store_id is not string %}IN ({{ store_id | join(", ") }}){% else %}= {{ store_id }}{% endif %}
+    {% endif %}
+    {% if city %}
+      AND s.district_name = '{{ city }}'
+    {% endif %}
+      AND r.line_item_id IN (
+            SELECT item AS line_item_id
+            FROM assortment_ods.items
+            WHERE 1=1
+              AND item_desc NOT LIKE 'CR_%'
+              AND gamma IN ('A', 'Ac', 'A0', 'B')
+              AND line_item_id::int NOT IN ({{ checkout_items | join(", ") }})
+              AND subdept <> 940
+       )
+    GROUP BY
+        r.receipt_surrogate_id,
+        r.line_item_id,
+        r.client_type,
+        r.client_loyalty_card_number,
+        r.opened_date,
+        r.store_id,
+        i.model_id,
+        i.dept,
+        i.subdept,
+        i.class_id,
+        i.subclass,
+        toYYYYMM(r.opened_date),
+        it.total_turnover,
+        st.store_turnover
+)
+WHERE count_items >= 3;
